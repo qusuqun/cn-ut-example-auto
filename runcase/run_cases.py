5397a0831b425b625e7862d70a9b9a2d865b4009
DW Lucy Qu - SZ CN <lucy.qu@danielwellington.com>
2020-06-12 11:07:40 +0800

    first commit

---
+++ runcase/run_cases.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append(r'D:\selenium_python\Python\cn-unittest-example-automation')
+from test_method import TestCount
+import os
+import time
+import unittest
+import HTMLTestRunner
+
+# 方法一：
+pro_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(pro_dir, "src")
+print(path)
+discover = unittest.defaultTestLoader.discover(path, pattern="test*.py", top_level_dir=None)
+print(discover)
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(discover)
+
+# 方法二：
+suite = unittest.makeSuite(TestCount, "test")
+all_test = unittest.TestSuite(suite)
+runner = unittest.TextTestRunner()
+runner.run(all_test)
+
+
+# 方法三：
+suite = unittest.makeSuite(TestCount, "test")
+runner = unittest.TextTestRunner()
+runner.run(suite)
+
+# 方法四：
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(TestCount("test_add"))
+    suite.addTest(TestCount("test_sub"))
+    suite.addTest(TestCount("test_mul"))
+    suite.addTest(TestCount("test_div"))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+# 方法五：
+now = time.strftime("%Y%m%d_%H%M%S")
+pro_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+report_name = now + " " + "testReport.html"
+report_path = os.path.join(pro_dir, "testresult", report_name)
+outfile = open(report_path, "wb")
+runner = HTMLTestRunner.HTMLTestRunner(stream=outfile, title='test report',
+                                               description='calculator test report')
+suite = unittest.makeSuite(TestCount, "test")
+runner.run(suite)
+outfile.close()
---
+++ src/test_method.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append(r'D:\selenium_python\Python\cn-unittest-example-automation')
+from calculator import Count
+import unittest
+
+class TestCount(unittest.TestCase):
+    # 测试的初始化
+    def setUp(self):
+        print("test start")
+
+    #测试用例add的编写
+    def test_add(self):
+        self.Count = Count(2, 3)
+        self.assertEqual(self.Count.add(), 5)
+
+    # 测试用例sub的编写
+    def test_sub(self):
+        self.Count = Count(6, 3)
+        self.assertEqual(self.Count.sub(), 3)
+
+    # 测试用例mul的编写
+    def test_mul(self):
+        self.Count = Count(6, 3)
+        self.assertEqual(self.Count.mul(), 18)
+
+    # 测试用例div的编写
+    def test_div(self):
+        self.Count = Count(6, 3)
+        self.assertEqual(self.Count.div(), 2)
+
+    # 测试的回收-测试的环境还原
+    def tearDown(self):
+        print("test end")
+
